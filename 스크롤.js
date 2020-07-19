e524da679a8fddedd471c7ba011d53791c82a9bd
Edward-Shawn <61370305+Edward-Shawn@users.noreply.github.com>
2020-07-19 21:25:32 +0900

    스크롤페이지에 스크롤시 헤더 감추기 기능 추가

--- 스크롤.js
+++ 스크롤.js
@@ -1,20 +1,52 @@
 $(document).ready(function(){
 
-
-    $(window).scroll(function(){
-        $("header").css("position","fixed");
-    })
+    var lastScrollTop = 0;
+    var delta = 5;
+    var didScroll;
+    var navbarHeight = $('header').outerHeight();
 
     
 
-    $(".menu li").click(function(){
-       
-        var i = $(this).index();
-        var ht = $("section > div").height();
-        var navPos = i * ht;
-       
-        $('html,body').animate({scrollTop: i * ht});
+    $(window).scroll(function(event){
+        didScroll = true;
+        $("header").css("position","fixed");
     })
+
+    setInterval(function(){
+        if(didScroll){
+            hasScrolled();
+            didScroll = false;
+        }
+    },250);
+
+    function hasScrolled(){
+        var st = $(this).scrollTop();
+        if(Math.abs(lastScrollTop - st) <= delta)
+        return;
+
+        if (st > lastScrollTop && st > navbarHeight){ 
+
+           $('header').removeClass('nav-down').addClass('nav-up'); 
+
+        } else { 
+
+            if(st + $(window).height() < $(document).height()) { 
+                $('header').removeClass('nav-up').addClass('nav-down'); 
+            } 
+        }
+
+        lastScrollTop = st;
+    }
+    
+
+    // $(".menu li").click(function(){
+       
+    //     var i = $(this).index();
+    //     var ht = $("section > div").height();
+    //     var navPos = i * ht;
+       
+    //     $('html,body').animate({scrollTop: i * ht});
+    // })
 
     $(".topBtn").click(function(){
         $('html,body').animate({scrollTop:0},500);
